0becb5fc8528a83943e0dbf1ab7ed04ab971d20b
QuentinTL <q.thibaut.lazaro96@gmail.com>
2023-07-02 00:02:24 +0200

    vérification par token et debut multer
    La vérification par token est faite et le pluggin multer installé

--- back-end/routes/sauce.js
+++ back-end/routes/sauce.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
 
 // Envoie d'une nouvelle sauce via le formulaire
-router.post('/', sauceCtrl.createSauce);
+router.post('/', auth, sauceCtrl.createSauce);
 
 // Utilisation de PUT afin de modifié une sauce en fonction de son id
-router.put('/:id', sauceCtrl.modifySauce);
+router.put('/:id', auth, sauceCtrl.modifySauce);
 
 // Delete pour supprimer une sauce selon son id
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 // Get pour révupérer seulement une sauce via son id dans notre de base de données
-router.get('/:id', sauceCtrl.displayOneSauce);
+router.get('/:id', auth, sauceCtrl.displayOneSauce);
 
 // get pour récupérer la liste complete de sauces dans notre base de donnée
-router.get('/', sauceCtrl.displayAllSauces);
+router.get('/', auth, sauceCtrl.displayAllSauces);
 
 module.exports = router;
